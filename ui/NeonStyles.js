c07c37ac53f1c2f1d06a442aa16246b33b407afa
timp-blimp <1059558@lwsd.org>
2026-03-05 10:28:38 -0800

    Expand neonColors and add applyNeonGlow function

--- ui/NeonStyles.js
+++ ui/NeonStyles.js
@@ -1,2 +1,15 @@
 // ui/NeonStyles.js
-export const neonColors = { blue: '#39f', green: '#0f9', yellow: '#ffb400' };
+
+export const neonColors = {
+  blue: '#39f',
+  green: '#0f9',
+  yellow: '#ffd000',
+  orange: '#ff7a00',
+  purple: '#b026ff',
+  red: '#ff2b2b'
+};
+
+export function applyNeonGlow(ctx, color, intensity = 20) {
+  ctx.shadowColor = color;
+  ctx.shadowBlur = intensity;
+}
