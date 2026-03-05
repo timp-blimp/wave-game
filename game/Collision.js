802a546db541d24143e868fd17a9eb946f1bd483
timp-blimp <1059558@lwsd.org>
2026-03-05 11:00:43 -0800

    Update Collision.js

--- game/Collision.js
+++ game/Collision.js
@@ -1,2 +1,24 @@
 // game/Collision.js
-export default class Collision { /* Step 5 */ }
+
+export default class Collision {
+  constructor(margin = 6) {
+    this.margin = margin;
+  }
+
+  check(player, terrain) {
+    const bounds = terrain.getCurrentBounds(player.x);
+    if (!bounds) return false;
+
+    const { topY, bottomY } = bounds;
+
+    if (player.y - player.size + this.margin <= topY) {
+      return true;
+    }
+
+    if (player.y + player.size - this.margin >= bottomY) {
+      return true;
+    }
+
+    return false;
+  }
+}
