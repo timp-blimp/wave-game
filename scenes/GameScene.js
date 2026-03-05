dc3418c46c7a87a38e46d5d4b10d7aceb7dcfa93
timp-blimp <1059558@lwsd.org>
2026-03-05 10:32:40 -0800

    Implement GameScene class with player functionality

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -1,2 +1,40 @@
 // scenes/GameScene.js
-export default class GameScene { /* implemented in Step 5 */ }
+
+import WavePlayer from '../game/WavePlayer.js';
+import { neonColors } from '../ui/NeonStyles.js';
+
+export default class GameScene {
+  constructor() {
+    this.canvas = null;
+    this.ctx = null;
+    this.player = null;
+  }
+
+  enter(data = {}) {
+    this.canvas = document.getElementById('game-canvas');
+    this.ctx = this.canvas.getContext('2d');
+
+    this.player = new WavePlayer({
+      x: 150,
+      y: this.canvas.height / 2,
+      color: neonColors.blue,
+      controlKey: 'ArrowRight'
+    });
+  }
+
+  exit() {}
+
+  update(dt) {
+    this.player.update(dt);
+  }
+
+  render(ctx) {
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    // Background
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.player.render(ctx);
+  }
+}
